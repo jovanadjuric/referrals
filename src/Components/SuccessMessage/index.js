385458517ab691a51ae2aac3fbbe76413ca4df92
jovanamatk <mat.jovana@gmail.com>
2022-01-11 13:36:57 +0100

    feat: do the styling of the success message

--- src/Components/SuccessMessage/index.js
+++ src/Components/SuccessMessage/index.js
@@ -2,8 +2,9 @@ const SuccessMessage = ({ count }) => {
   return (
     <div className="alert alert-success" role="alert">
       <p>
-        Success! You have submitted {count} pending referrals. You will be
-        notified once they've been approved
+        Success! You have submitted {count} pending referral
+        {count === 1 ? "" : "s"}. You will be notified once they've been
+        approved
       </p>
     </div>
   );
